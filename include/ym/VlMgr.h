107c00f6ab6c292c1ae9b6611cc26f6bc1c19ccc
Yusuke Matsunaga <yusuke_matsunaga@ieee.org>
2016-02-14 12:19:58 +0900

    ﻿VlMgr.h: コメントアウトしている部分を削除した．

--- include/ym/VlMgr.h
+++ include/ym/VlMgr.h
@@ -115,17 +115,6 @@ public:
   find_obj(const VlNamedObj* parent,
 	   const char* name) const;
 
-#if 0
-  /// @brief スコープに属する generate block のリストを取り出す．
-  /// @param[in] parent 検索対象のスコープ
-  /// @param[out] scope_list 結果を格納するリスト
-  /// @retval true 該当する要素が1つ以上あった．
-  /// @retval false 該当する要素がなかった．
-  bool
-  find_genblock_list(const VlNamedObj* parent,
-		     vector<const VlNamedObj*>& scope_list) const;
-#endif
-
   /// @brief スコープに属する internal scope のリストを取り出す．
   /// @param[in] parent 検索対象のスコープ
   /// @param[out] scope_list 結果を格納するリスト
